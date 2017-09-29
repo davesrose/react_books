05bcbe3652e04255c6502b2de7bdac5a048353be
davesrose <dsr0116@yahoo.com>
2017-09-28 21:16:08 -0400

    updated server

--- server.js
+++ server.js
@@ -16,13 +16,13 @@ app.use(routes);
 // Set up promises with mongoose
 mongoose.Promise = global.Promise;
 // Connect to the Mongo DB
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist",
-  {
-    useMongoClient: true
-  }
-);
 
+if(process.env.NODE_ENV == 'production'){
+  mongoose.connect('mongodb://heroku_mkmpv55c:auvt3hlid14hlfg7bqacad2v1h@ds013946.mlab.com:13946/heroku_mkmpv55c');
+}
+else{
+  mongoose.connect('mongodb://localhost/news-scraper');
+}
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
